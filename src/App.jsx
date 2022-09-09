cba5faf61a8a7b4f3872b3441f19e8e47d6eea5d
JAL <jalejandromogro1@gmail.com>
2022-09-09 15:09:42 -0400

    comenzando Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 
-function App() {
+function Portfolio() {
 
   return (
-    <div className="App px-5 ">
+    <div className="Portfolio px-5 ">
       <Header/>
       <Hero/>
     </div>
   )
 }
 
-export default App
+export default Portfolio
